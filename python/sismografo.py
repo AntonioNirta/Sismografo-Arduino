f1386c965b3b9f71074f5ea96fe13b32f0a29d23
Francesco Apruzzese <cescoap@gmail.com>
2020-01-11 10:19:41 +0100

    [FIX] Variable name shadow base library

--- python/sismografo.py
+++ python/sismografo.py
@@ -15,16 +15,19 @@ def main():
     AcZ = db.iloc[:, 2]
 
     # Time settings
-    time = db.iloc[:, 3]  # time in milliseconds, give by arduino
+    arduino_time = db.iloc[:, 3]  # time in milliseconds, give by arduino
 
-    tempo = [i/1000 for i in time]  # time in seconds
+    converted_time = [i/1000 for i in arduino_time]  # time in seconds
 
     # Create plots with pre-defined labels.
     fig, ax = plt.subplots()
 
-    ax.plot(tempo, AcX, '-', color="red", linewidth=0.8, label='AcX')
-    ax.plot(tempo, AcY, '-', color="blue", linewidth=0.8, label='AcY')
-    ax.plot(tempo, AcZ, '-', color="green", linewidth=0.8, label='AcZ')
+    ax.plot(converted_time, AcX, '-',
+            color="red", linewidth=0.8, label='AcX')
+    ax.plot(converted_time, AcY, '-',
+            color="blue", linewidth=0.8, label='AcY')
+    ax.plot(converted_time, AcZ, '-',
+            color="green", linewidth=0.8, label='AcZ')
 
     legend = fig.legend(loc='upper right', shadow=True, fontsize='medium')
 
